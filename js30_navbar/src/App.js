f16ced557b8b332bc86e39fa46a83178e7940dbd
enkhhuslen <enkhhuslenbatmunkh@gmail.com>
2024-01-30 20:30:56 +0800

    minor change on js30

--- js30_navbar/src/App.js
+++ js30_navbar/src/App.js
@@ -6,7 +6,7 @@ import Clients from './components/Clients';
 
 function App() {
   return (
-    <div className="bg-primary w-full h-full overflow-hidden">
+    <div className="bg-primary w-full h-screen overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
